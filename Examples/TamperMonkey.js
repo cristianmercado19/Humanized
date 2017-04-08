8619834d92cd704e11d319c49369981ad8fac4d3
Cristian <cristianmercado19@gmail.com>
2017-04-09 00:21:04 +0100

    Adding Tampermonkey script example

---
+++ Examples/TamperMonkey.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Humanized
+// @namespace    https://github.com/cristianmercado19
+// @version      1.0
+// @description  Shortcuts auto-generated for any web page... Inspired by Humanized Enso Launcher
+// @author       Cristian Mercado
+// @match        http://*/*
+// @match        https://*/*
+// @require      https://raw.githubusercontent.com/cristianmercado19/Humanized/master/src/Humanized.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+   
+    Humanized.Initializer.init();
+
+})();
